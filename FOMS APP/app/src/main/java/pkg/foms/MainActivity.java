c93a586964a97109eb0dccc621566093dec11d9a
M.ROHAN FAROOQUI <31238460+RohanFarooqui@users.noreply.github.com>
2022-03-02 18:30:32 +0500

    Update

---
+++ FOMS APP/app/src/main/java/pkg/foms/MainActivity.java
@@ -0,0 +1,42 @@
+package pkg.foms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import pkg.foms.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Handler handler;
+    private Runnable callback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        handler = new Handler();
+        callback = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        };
+        handler.postDelayed(callback, 10000);
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(callback);
+    }
+}
